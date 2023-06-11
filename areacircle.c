7752d4fc2b32b089c5c7447842d3b8478e89dc73
Nabila Tabassum <133186987+nabila200000@users.noreply.github.com>
2023-06-11 23:48:43 +0600

    Add files via upload

---
+++ areacircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float r,area ;
+    printf("Enter the value of radius");
+    scanf("%f",&r);
+
+    area=3.1416*r*r;
+    printf("The area is=%f\n",area);
+    return 0;
+}
